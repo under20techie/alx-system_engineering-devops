1e48d0dc7ddc9f802db41ffa8cf3bd5d8902f43d
Your Name <you@example.com>
2024-02-02 19:51:18 +0100

    you

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -10,7 +10,7 @@ int main(void)
 
 	while (i < 5 && fork() > 0)
 	{
-		printf("Zombie process created, %d: %d", getppid(), getpid());
+		printf("Zombie process created, PID: %d", getpid());
 		i++;
 	}
 	infinite_while();
